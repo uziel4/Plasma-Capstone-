db875f204ef19e6bdc18f1434080019fff8172a7
Uziel E. Santos Rodriguez <uziel-eli@hotmail.es>
2026-06-21 19:20:29 -0400

    Delete New_GUI_AND Automation Implementation 8 relayplate directory

--- New_GUI_AND Automation Implementation 8 relayplate/widgets.py
+++
@@ -1,5 +0,0 @@
-def set_button_state(button, active, colors):
-    button.config(
-        highlightbackground=colors["red"] if active else colors["grid"],
-        highlightcolor=colors["red"] if active else colors["grid"]
-    )
